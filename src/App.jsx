08fc04ce3a70abf1b24ca1b753d7c69419974828
vaskar-ghosh <vaskar.ghosh.99.23@gmail.com>
2024-03-31 22:43:33 +0600

    Work of nav & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 /** @format */
 
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import Investments from "./pages/Investments";
+
 function App() {
-  return <div className="text-5xl text-blue-500">Hello </div>;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/investments" element={<Investments />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
